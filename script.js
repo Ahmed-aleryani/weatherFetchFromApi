849fd636c97f18a85c397b2072e9749d3c3d7731
ahmed aleryani <ahmed.m.aleryani@gmail.com>
2020-08-31 08:14:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+window.addEventListener("DOMContentLoaded",function(){
+
+  var searchForm=document.getElementById("searchForm");
+
+  searchForm.addEventListener("submit",function(event){
+    event.preventDefault();
+    let cityName=document.getElementById("city");
+    if(cityName.value!="")
+    queryCity(cityName.value);
+    else{
+      alert("City name can not be empty");
+      return;
+    }
+  })
+
+  function queryCity(cityName){
+      fetch(`https://www.metaweather.com/api/location/search/?query=${cityName}`).then(data=>data.json())
+      .then(element=>console.log(element.woeid))
+  }
+// id="searchForm">
+//       <input type="text" id='city'
+
+  queryCity("tokyo")
+})
+
+_cityWether  = ( cityId , date) =>{
+  fetch(`https://www.metaweather.com/api/location/${cityId}/${data}/`)
+  .then(respnse => respnse.json())
+  .then(data => {
+    return()
+  }).catch(e => {
+    console.log(e)
+  })
+
+}
